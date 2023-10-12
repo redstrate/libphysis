aff19962aeb5d5ab7365c3e610a47ce5ab74d7cd
Joshua Goins <josh@redstrate.com>
2023-10-12 19:01:11 -0400

    Add physis_index_parse and physis_generate_partial_hash
    To be used in Sagasu for indexing files

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,8 @@ use tracing_subscriber::Layer;
 use tracing_subscriber::prelude::*;
 use std::fmt::Write;
 use physis::cfg::ConfigFile;
+use physis::index::IndexFile;
+use physis::sqpack::calculate_partial_hash;
 
 type LogCallback = unsafe extern "C" fn(QtMsgType, *const c_char, *const c_char, i32);
 
@@ -972,4 +974,47 @@ pub struct physis_ConfigFile {
 
         leak_buffer
     }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+#[cfg(feature = "visual_data")]
+pub struct physis_IndexEntries {
+    num_entries: u32,
+    dir_entries: *const u32,
+    filename_entries: *const u32
+}
+
+#[cfg(feature = "visual_data")]
+#[no_mangle] pub extern "C" fn physis_index_parse(path: *const c_char) -> physis_IndexEntries {
+    if let Some(idx_file) = IndexFile::from_existing(&ffi_from_c_string(path)) {
+        let mut c_dir_entries = vec![];
+        let mut c_file_entries = vec![];
+
+        for entry in &idx_file.entries {
+            c_file_entries.push(entry.hash as u32);
+            c_dir_entries.push(((entry.hash >> 32) as u32));
+        }
+
+        let mat = physis_IndexEntries {
+            num_entries: c_dir_entries.len() as u32,
+            dir_entries: c_dir_entries.as_mut_ptr(),
+            filename_entries: c_file_entries.as_mut_ptr()
+        };
+
+        mem::forget(c_dir_entries);
+        mem::forget(c_file_entries);
+
+        mat
+    } else {
+        physis_IndexEntries {
+            num_entries: 0,
+            dir_entries: null_mut(),
+            filename_entries: null_mut()
+        }
+    }
+}
+
+#[no_mangle] pub extern "C" fn physis_generate_partial_hash(name: *const c_char) -> u32 {
+    calculate_partial_hash(&ffi_from_c_string(name))
 }
