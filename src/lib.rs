e44fb6433086669500254f4deb1aceff7ab0fde2
Joshua Goins <josh@redstrate.com>
2023-10-12 21:31:38 -0400

    Add EXL parsing function

--- src/lib.rs
+++ src/lib.rs
@@ -1021,3 +1021,46 @@ pub struct physis_IndexEntries {
 #[no_mangle] pub extern "C" fn physis_generate_partial_hash(name: *const c_char) -> u32 {
     calculate_partial_hash(&ffi_from_c_string(name))
 }
+
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct physis_EXL {
+    version: i32,
+    entry_count: i32,
+    entry_keys: *const *const c_char,
+    entry_values: *const i32
+}
+
+#[no_mangle] pub extern "C" fn physis_gamedata_read_excel_list(buffer : physis_Buffer) -> physis_EXL {
+    let data = unsafe { slice::from_raw_parts(buffer.data, buffer.size as usize) };
+
+    if let Some(exl) = EXL::from_existing(&data.to_vec()) {
+        let mut c_keys = vec![];
+        let mut c_values = vec![];
+
+        for (key, value) in &exl.entries {
+            c_keys.push(ffi_to_c_string(key));
+            c_values.push(*value);
+        }
+
+        let mat = physis_EXL {
+            version: exl.version,
+            entry_count: c_keys.len() as i32,
+            entry_keys: c_keys.as_mut_ptr(),
+            entry_values: c_values.as_mut_ptr()
+        };
+
+        mem::forget(c_keys);
+        mem::forget(c_values);
+
+        mat
+    } else {
+        physis_EXL {
+            version: 0,
+            entry_count: 0,
+            entry_keys: null_mut(),
+            entry_values: null_mut()
+        }
+    }
+}
