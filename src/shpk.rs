991146eeff4c154a6206d58a9f17b4ecea517d58
Joshua Goins <josh@redstrate.com>
2024-04-20 17:24:57 -0400

    Expose more SHPK internals
    Needed for future Novus things

--- src/shpk.rs
+++ src/shpk.rs
@@ -1,23 +1,34 @@
 // SPDX-FileCopyrightText: 2024 Joshua Goins <josh@redstrate.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use crate::physis_Buffer;
-use physis::shpk::ShaderPackage;
-use std::ptr::null_mut;
+use crate::{ffi_to_c_string, physis_Buffer};
+use physis::shpk::{Pass, ResourceParameter, ShaderPackage};
+use std::ptr::{null, null_mut};
 use std::{mem, slice};
+use std::ffi::c_char;
 
 #[repr(C)]
 #[derive(Clone, Copy)]
-#[cfg(feature = "visual_data")]
-pub struct physis_Shader {
-    len: i32,
-    bytecode: *mut u8,
+pub struct physis_ShaderParameter {
+    slot: u16,
+    name: *const c_char
 }
 
 #[repr(C)]
 #[derive(Clone, Copy)]
-#[cfg(feature = "visual_data")]
+pub struct physis_Shader {
+    len: i32,
+    bytecode: *mut u8,
+    num_scalar_parameters: i32,
+    scalar_parameters: *mut physis_ShaderParameter,
+    num_resource_parameters: i32,
+    resource_parameters: *mut physis_ShaderParameter,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
 pub struct physis_SHPK {
+    p_ptr: *mut ShaderPackage,
     num_vertex_shaders: i32,
     vertex_shaders: *mut physis_Shader,
     num_pixel_shaders: i32,
@@ -27,6 +38,7 @@ pub struct physis_SHPK {
 impl Default for physis_SHPK {
     fn default() -> Self {
         Self {
+            p_ptr: null_mut(),
             num_vertex_shaders: 0,
             vertex_shaders: null_mut(),
             num_pixel_shaders: 0,
@@ -35,7 +47,23 @@ impl Default for physis_SHPK {
     }
 }
 
-#[cfg(feature = "visual_data")]
+fn physis_get_shader_parameter_array(parameterArray: &Vec<ResourceParameter>) -> (i32, *mut physis_ShaderParameter) {
+    let mut vec = vec![];
+
+    for resource in parameterArray {
+        vec.push(physis_ShaderParameter {
+            name: ffi_to_c_string(&resource.name),
+            slot: resource.slot
+        })
+    }
+
+    let result = (vec.len() as i32, vec.as_mut_ptr());
+
+    mem::forget(vec);
+
+    result
+}
+
 #[no_mangle]
 pub extern "C" fn physis_parse_shpk(buffer: physis_Buffer) -> physis_SHPK {
     let data = unsafe { slice::from_raw_parts(buffer.data, buffer.size as usize) };
@@ -44,12 +72,19 @@ pub extern "C" fn physis_parse_shpk(buffer: physis_Buffer) -> physis_SHPK {
         let mut c_vertex_shaders = vec![];
         let mut c_fragment_shaders = vec![];
 
-        for shader in shpk.vertex_shaders {
+        for shader in &shpk.vertex_shaders {
             let mut bytecode = shader.bytecode.clone();
+
+            let (num_scalar_params, scalar_params) = physis_get_shader_parameter_array(&shader.scalar_parameters);
+            let (num_resource_params, resource_params) = physis_get_shader_parameter_array(&shader.resource_parameters);
 
             let shader = physis_Shader {
                 len: bytecode.len() as i32,
                 bytecode: bytecode.as_mut_ptr(),
+                num_scalar_parameters: num_scalar_params,
+                scalar_parameters: scalar_params,
+                num_resource_parameters: num_resource_params,
+                resource_parameters: resource_params,
             };
 
             c_vertex_shaders.push(shader);
@@ -57,12 +92,19 @@ pub extern "C" fn physis_parse_shpk(buffer: physis_Buffer) -> physis_SHPK {
             mem::forget(bytecode);
         }
 
-        for shader in shpk.pixel_shaders {
+        for shader in &shpk.pixel_shaders {
             let mut bytecode = shader.bytecode.clone();
+
+            let (num_scalar_params, scalar_params) = physis_get_shader_parameter_array(&shader.scalar_parameters);
+            let (num_resource_params, resource_params) = physis_get_shader_parameter_array(&shader.resource_parameters);
 
             let shader = physis_Shader {
                 len: bytecode.len() as i32,
                 bytecode: bytecode.as_mut_ptr(),
+                num_scalar_parameters: num_scalar_params,
+                scalar_parameters: scalar_params,
+                num_resource_parameters: num_resource_params,
+                resource_parameters: resource_params,
             };
 
             c_fragment_shaders.push(shader);
@@ -71,6 +113,7 @@ pub extern "C" fn physis_parse_shpk(buffer: physis_Buffer) -> physis_SHPK {
         }
 
         let mat = physis_SHPK {
+            p_ptr: Box::leak(Box::new(shpk)),
             num_vertex_shaders: c_vertex_shaders.len() as i32,
             vertex_shaders: c_vertex_shaders.as_mut_ptr(),
             num_pixel_shaders: c_fragment_shaders.len() as i32,
@@ -83,5 +126,72 @@ pub extern "C" fn physis_parse_shpk(buffer: physis_Buffer) -> physis_SHPK {
         mat
     } else {
         physis_SHPK::default()
+    }
+}
+
+
+#[repr(C)]
+pub struct physis_SHPKNode {
+    selector: u32,
+    pass_count: u32,
+    pass_indices: [u8; 16],
+    system_key_count: u32,
+    system_keys: *mut u32,
+    scene_key_count: u32,
+    scene_keys: *mut u32,
+    material_key_count: u32,
+    material_keys: *mut u32,
+    subview_key_count: u32,
+    subview_keys: *mut u32,
+    passes: *mut Pass
+}
+
+#[no_mangle] pub extern "C" fn physis_shpk_get_node(shpk : *const physis_SHPK, key: u32) -> physis_SHPKNode {
+    unsafe {
+        if let Some(node) = (*(*shpk).p_ptr).find_node(key) {
+            let mut c_system_keys = node.system_keys.clone();
+            let mut c_scene_keys = node.scene_keys.clone();
+            let mut c_material_keys = node.material_keys.clone();
+            let mut c_subview_keys = node.subview_keys.clone();
+            let mut c_passes = node.passes.clone();
+
+            let new_node = physis_SHPKNode {
+                selector: node.selector,
+                pass_count: node.pass_count,
+                pass_indices: node.pass_indices,
+                system_key_count: node.system_keys.len() as u32,
+                system_keys: c_system_keys.as_mut_ptr(),
+                scene_key_count: node.scene_keys.len() as u32,
+                scene_keys: c_scene_keys.as_mut_ptr(),
+                material_key_count: node.material_keys.len() as u32,
+                material_keys: c_material_keys.as_mut_ptr(),
+                subview_key_count: node.subview_keys.len() as u32,
+                subview_keys: c_subview_keys.as_mut_ptr(),
+                passes: c_passes.as_mut_ptr(),
+            };
+
+            mem::forget(c_system_keys);
+            mem::forget(c_scene_keys);
+            mem::forget(c_material_keys);
+            mem::forget(c_subview_keys);
+            mem::forget(c_passes);
+
+            new_node
+        } else {
+            physis_SHPKNode {
+                selector: 0,
+                pass_count: 0,
+                pass_indices: [0; 16],
+                system_key_count: 0,
+                system_keys: null_mut(),
+                scene_key_count: 0,
+                scene_keys: null_mut(),
+                material_key_count: 0,
+                material_keys: null_mut(),
+                subview_key_count: 0,
+                subview_keys: null_mut(),
+                passes: null_mut(),
+            }
+        }
     }
 }
