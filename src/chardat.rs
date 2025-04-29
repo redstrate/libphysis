806395eb4c6e76f9fbe8fb7ed4e2d864bdb1c522
Joshua Goins <josh@redstrate.com>
2025-04-29 15:48:03 -0400

    Update chardat bindings to new Physis API

--- src/chardat.rs
+++ src/chardat.rs
@@ -1,13 +1,29 @@
 // SPDX-FileCopyrightText: 2024 Joshua Goins <josh@redstrate.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use crate::physis_Buffer;
-use physis::savedata::chardat::CharacterData;
+use crate::{ffi_to_c_string, physis_Buffer};
+use physis::savedata::chardat::{CharacterData, CustomizeData};
 use std::slice;
+use std::ffi::c_char;
+
+#[repr(C)]
+pub struct physis_CharacterData {
+    version: u32,
+    customize: CustomizeData,
+    timestamp: u32,
+    comment: *const c_char,
+}
 
 #[unsafe(no_mangle)]
-pub extern "C" fn physis_chardat_parse(buffer: physis_Buffer) -> CharacterData {
+pub extern "C" fn physis_chardat_parse(buffer: physis_Buffer) -> physis_CharacterData {
     let data = unsafe { slice::from_raw_parts(buffer.data, buffer.size as usize) };
 
-    CharacterData::from_existing(data).unwrap()
+    let chardat = CharacterData::from_existing(data).unwrap();
+
+    physis_CharacterData {
+        version: chardat.version,
+        customize: chardat.customize,
+        timestamp: chardat.timestamp,
+        comment: ffi_to_c_string(&chardat.comment),
+    }
 }
