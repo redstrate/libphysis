38eb305ebeae4ff4f44595ec13fb62513cf5b653
Joshua Goins <josh@redstrate.com>
2025-11-09 11:47:05 -0500

    Expose Excel page information

--- src/exh.rs
+++ src/exh.rs
@@ -15,11 +15,20 @@ pub struct physis_ColumnDefinition {
     offset: u16,
 }
 
+// TODO: re-use from Physis since their struct is also simple
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct physis_ExcelPage {
+    start_id: u32,
+    row_count: u32,
+}
+
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct physis_EXH {
     pub(crate) p_ptr: *mut EXH,
     page_count: u32,
+    pages: *mut physis_ExcelPage,
     language_count: u32,
     languages: *mut Language,
     column_count: u32,
@@ -53,6 +62,15 @@ pub extern "C" fn physis_parse_excel_sheet_header(buffer: physis_Buffer) -> *mut
         });
     }
 
+    let mut c_pages: Vec<physis_ExcelPage> = vec![];
+
+    for page in &exh.pages {
+        c_pages.push(physis_ExcelPage {
+            start_id: page.start_id,
+            row_count: page.row_count,
+        });
+    }
+
     let page_len = exh.pages.len() as u32;
     let row_count = exh.header.row_count as u32;
     let column_count = exh.column_definitions.len() as u32;
@@ -65,10 +83,12 @@ pub extern "C" fn physis_parse_excel_sheet_header(buffer: physis_Buffer) -> *mut
         column_count,
         row_count,
         column_definitions: c_column_definitions.as_mut_ptr(),
+        pages: c_pages.as_mut_ptr(),
     };
 
     mem::forget(c_languages);
     mem::forget(c_column_definitions);
+    mem::forget(c_pages);
 
     Box::leak(Box::new(repositories))
 }
