1f3944b356913546d5cef34957e1f34002590a86
Joshua Goins <josh@redstrate.com>
2026-01-05 17:36:07 -0500

    Update to new Physis Excel API

--- src/resource.rs
+++ src/resource.rs
@@ -1,11 +1,13 @@
 // SPDX-FileCopyrightText: 2026 Joshua Goins <josh@redstrate.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use crate::exd::{physis_ColumnData, physis_ExcelRow, physis_ExcelRows};
+use crate::exd::{physis_ExcelEntry, physis_ExcelRow, physis_Field};
 use crate::exh::physis_EXH;
 use crate::{ffi_free_string, ffi_from_c_string, ffi_to_c_string, ffi_to_vec, physis_Buffer};
 use physis::common::{Language, Platform};
-use physis::excel::{ColumnData, ExcelRowKind, ExcelSheet};
+use physis::excel::Field;
+use physis::excel::Row;
+use physis::excel::{Entry, Sheet};
 use physis::repository::RepositoryType;
 use physis::resource::{RepairAction, Resource, SqPackRelease, SqPackResource};
 use physis::sqpack::Hash;
@@ -99,32 +101,32 @@ pub extern "C" fn physis_sqpack_read(
 
 #[repr(C)]
 pub struct physis_ExcelSheetPage {
-    pub row_count: c_uint,
-    pub rows: *const physis_ExcelRows,
+    pub entry_count: c_uint,
+    pub entries: *const physis_ExcelEntry,
     pub column_count: c_uint,
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn physis_excel_page_free_rows(
     page: &physis_ExcelSheetPage,
-    rows: &physis_ExcelRows,
+    rows: &physis_ExcelEntry,
 ) {
     unsafe {
         let data = Vec::from_raw_parts(
-            rows.row_data,
-            rows.row_count as usize,
-            rows.row_count as usize,
+            rows.subrows,
+            rows.subrow_count as usize,
+            rows.subrow_count as usize,
         );
 
-        for i in 0..rows.row_count {
+        for i in 0..rows.subrow_count {
             let col_data = Vec::from_raw_parts(
-                data[i as usize].column_data,
+                data[i as usize].columns,
                 page.column_count as usize,
                 page.column_count as usize,
             );
 
             for col in &col_data {
-                if let physis_ColumnData::String(s) = col {
+                if let physis_Field::String(s) = col {
                     ffi_free_string(*s);
                 }
             }
@@ -138,7 +140,7 @@ pub extern "C" fn physis_excel_page_free_rows(
 
 #[repr(C)]
 pub struct physis_ExcelSheet {
-    p_ptr: *mut ExcelSheet,
+    p_ptr: *mut Sheet,
     page_count: u32,
     pages: *const physis_ExcelSheetPage,
 }
@@ -153,52 +155,51 @@ impl Default for physis_ExcelSheet {
     }
 }
 
-fn to_c_row(row_id: u32, row_kind: &ExcelRowKind) -> physis_ExcelRows {
-    let reduced_rows = match row_kind {
-        ExcelRowKind::SingleRow(val) => &vec![(0u16, val.clone())],
-        ExcelRowKind::SubRows(rows) => rows,
-    };
-
+fn to_c_entry(row_id: u32, entry: &Entry) -> physis_ExcelEntry {
     let mut c_subrows: Vec<physis_ExcelRow> = Vec::new();
 
-    for (subrow_id, row) in reduced_rows {
-        let mut c_col_data: Vec<physis_ColumnData> = Vec::new();
-
-        for col_data in &row.columns {
-            match &col_data {
-                ColumnData::String(s) => {
-                    c_col_data.push(physis_ColumnData::String(ffi_to_c_string(s)))
-                }
-                ColumnData::Bool(b) => c_col_data.push(physis_ColumnData::Bool(*b)),
-                ColumnData::Int8(i) => c_col_data.push(physis_ColumnData::Int8(*i)),
-                ColumnData::UInt8(i) => c_col_data.push(physis_ColumnData::UInt8(*i)),
-                ColumnData::Int16(i) => c_col_data.push(physis_ColumnData::Int16(*i)),
-                ColumnData::UInt16(i) => c_col_data.push(physis_ColumnData::UInt16(*i)),
-                ColumnData::Int32(i) => c_col_data.push(physis_ColumnData::Int32(*i)),
-                ColumnData::UInt32(i) => c_col_data.push(physis_ColumnData::UInt32(*i)),
-                ColumnData::Float32(i) => c_col_data.push(physis_ColumnData::Float32(*i)),
-                ColumnData::Int64(i) => c_col_data.push(physis_ColumnData::Int64(*i)),
-                ColumnData::UInt64(i) => c_col_data.push(physis_ColumnData::UInt64(*i)),
-            }
-        }
-
-        c_subrows.push(physis_ExcelRow {
-            subrow_id: *subrow_id,
-            column_data: c_col_data.as_mut_ptr(),
-        });
-
-        mem::forget(c_col_data);
+    for (subrow_id, row) in &entry.subrows {
+        c_subrows.push(to_c_row(*subrow_id, row));
     }
 
-    let rows = physis_ExcelRows {
+    let rows = physis_ExcelEntry {
         row_id,
-        row_count: c_subrows.len() as u32,
-        row_data: c_subrows.as_mut_ptr(),
+        subrow_count: c_subrows.len() as u32,
+        subrows: c_subrows.as_mut_ptr(),
     };
 
     mem::forget(c_subrows);
 
     rows
+}
+
+fn to_c_row(subrow_id: u16, row: &Row) -> physis_ExcelRow {
+    let mut c_col_data: Vec<physis_Field> = Vec::new();
+
+    for col_data in &row.columns {
+        match &col_data {
+            Field::String(s) => c_col_data.push(physis_Field::String(ffi_to_c_string(s))),
+            Field::Bool(b) => c_col_data.push(physis_Field::Bool(*b)),
+            Field::Int8(i) => c_col_data.push(physis_Field::Int8(*i)),
+            Field::UInt8(i) => c_col_data.push(physis_Field::UInt8(*i)),
+            Field::Int16(i) => c_col_data.push(physis_Field::Int16(*i)),
+            Field::UInt16(i) => c_col_data.push(physis_Field::UInt16(*i)),
+            Field::Int32(i) => c_col_data.push(physis_Field::Int32(*i)),
+            Field::UInt32(i) => c_col_data.push(physis_Field::UInt32(*i)),
+            Field::Float32(i) => c_col_data.push(physis_Field::Float32(*i)),
+            Field::Int64(i) => c_col_data.push(physis_Field::Int64(*i)),
+            Field::UInt64(i) => c_col_data.push(physis_Field::UInt64(*i)),
+        }
+    }
+
+    let row = physis_ExcelRow {
+        subrow_id,
+        columns: c_col_data.as_mut_ptr(),
+    };
+
+    std::mem::forget(c_col_data);
+
+    row
 }
 
 #[unsafe(no_mangle)]
@@ -213,25 +214,24 @@ pub unsafe extern "C" fn physis_sqpack_read_excel_sheet(
             return physis_ExcelSheet::default();
         };
 
-        if let Ok(exd) = (*resource.p_ptr).read_excel_sheet((*exh.p_ptr).clone(), &r_name, language)
-        {
+        if let Ok(exd) = (*resource.p_ptr).read_excel_sheet(&*exh.p_ptr, &r_name, language) {
             let exd = Box::new(exd);
 
             let mut c_pages = Vec::new();
             for page in &exd.pages {
-                let mut c_rows = Vec::new();
+                let mut c_entries = Vec::new();
 
-                for row in &page.rows {
-                    c_rows.push(to_c_row(row.row_id, &row.kind));
+                for row in &page.entries {
+                    c_entries.push(to_c_entry(row.id, row));
                 }
 
                 let page = physis_ExcelSheetPage {
                     column_count: (*exh.p_ptr).column_definitions.len() as c_uint,
-                    row_count: page.rows.len() as u32,
-                    rows: c_rows.as_ptr(),
+                    entry_count: page.entries.len() as u32,
+                    entries: c_entries.as_ptr(),
                 };
 
-                mem::forget(c_rows);
+                mem::forget(c_entries);
 
                 c_pages.push(page);
             }
@@ -262,14 +262,14 @@ pub extern "C" fn physis_sqpack_free_excel_sheet(sheet: &physis_ExcelSheet) {
 pub unsafe extern "C" fn physis_excel_get_row(
     sheet: &physis_ExcelSheet,
     row_id: u32,
-) -> physis_ExcelRows {
+) -> physis_ExcelRow {
     unsafe {
-        if let Some(row_kind) = (*sheet.p_ptr).get_row(row_id) {
-            return to_c_row(row_id, &row_kind);
+        if let Some(row) = (*sheet.p_ptr).row(row_id) {
+            return to_c_row(0, &row);
         }
     }
 
-    physis_ExcelRows::default()
+    physis_ExcelRow::default()
 }
 
 #[repr(C)]
