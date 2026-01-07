1470899570f580d72a23f299fce08352c326f039
Joshua Goins <josh@redstrate.com>
2026-01-07 17:31:54 -0500

    Add missing REUSE information

--- src/sgb.rs
+++ src/sgb.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2026 Joshua Goins <josh@redstrate.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 use crate::physis_Buffer;
 use crate::scn::{physis_ScnSection, to_c_section};
 use physis::ReadableFile;
