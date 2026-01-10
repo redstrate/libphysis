1186bf2e7436b166eb4e9bd76171c84c6d9873b6
Joshua Goins <josh@redstrate.com>
2026-01-10 10:38:41 -0500

    Parse SHCDs for real

--- src/shcd.rs
+++ src/shcd.rs
@@ -1,24 +1,48 @@
 // SPDX-FileCopyrightText: 2024 Joshua Goins <josh@redstrate.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use physis::Platform;
+use crate::physis_Buffer;
 use physis::shcd::SHCD;
 use physis::shcd::ShaderStage;
+use physis::{Platform, ReadableFile};
 use std::ptr::null_mut;
-
-use crate::physis_Buffer;
+use std::slice;
 
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct physis_SHCD {
-    p_ptr: *mut SHCD,
-    shader_stage: ShaderStage,
+    stage: ShaderStage,
+    len: u32,
+    bytecode: *mut u8,
+}
+
+impl Default for physis_SHCD {
+    fn default() -> Self {
+        Self {
+            stage: ShaderStage::Vertex,
+            len: 0,
+            bytecode: null_mut(),
+        }
+    }
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn physis_shcd_parse(_platform: Platform, _buffer: physis_Buffer) -> physis_SHCD {
-    physis_SHCD {
-        p_ptr: null_mut(),
-        shader_stage: ShaderStage::Vertex,
+pub extern "C" fn physis_shcd_parse(platform: Platform, buffer: physis_Buffer) -> physis_SHCD {
+    let data = unsafe { slice::from_raw_parts(buffer.data, buffer.size as usize) };
+
+    if let Some(shcd) = SHCD::from_existing(platform, data) {
+        let mut c_bytecode = shcd.bytecode.clone();
+
+        let shcd = physis_SHCD {
+            stage: shcd.stage,
+            len: c_bytecode.len() as u32,
+            bytecode: c_bytecode.as_mut_ptr(),
+        };
+
+        std::mem::forget(c_bytecode);
+
+        shcd
+    } else {
+        physis_SHCD::default()
     }
 }
